affa3fe1b794c95f5379c0a056e1b5bea41bfa8f
Mr-Tech-13 <maxxshearey@hotmail.com>
2023-06-21 13:12:19 -0400

    Add selectors?

--- src/store/model/facebookmp.ts
+++ src/store/model/facebookmp.ts
@@ -5,11 +5,11 @@ export const FacebookMP: Store = {
     currency: '$',
     labels: {
         inStock: {
-            container: '',
-            text: [''],
+            container: 'div[class="x78zum5 xdt5ytf x1iyjqo2 x1t2pt76 xeuugli x1n2onr6 x1ja2u2z"]',
+            text: ['jetski'],
         },
         maxPrice: {
-            container: '',
+            container: 'div[class="x78zum5 x1q0g3np x1iorvi4 x4uap5 xjkvuk6 xkhd6sd"] span[class="x193iq5w xeuugli x13faqbe x1vvkbs xlh3980 xvmahel x1n0sxbx x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x3x7a5m x1lkfr7t x1lbecb7 x1s688f xzsf02u"]',
             euroFormat: false,
         },
     },
